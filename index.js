3bda9e1c7399c95d435c3fe7465edc6fd5e4bf20
Allan <allcreativewrites@gmail.com>
2023-09-18 13:02:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  let greeting;
+  if (rideDistance <= 400) {
+    greeting = 'This one is on me!';
+    return greeting;
+  }else if (rideDistance > 2000 && rideDistance < 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+    return greeting;
+  } else if (rideDistance > 2500) {
+    greeting= 'No can do.'
+    return greeting;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let message = (city === 'NYC') ? 'Ok, sounds good.': 'No go.';
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message;
+  switch (tip) {
+    case 'generous':
+      message = "Thank you so much."
+      break;
+    case 'not as generous':
+      message = 'Thank you.'
+      break;
+    default:
+      message ="Bye."
+      break;
+  }
+  return message;  
 }
